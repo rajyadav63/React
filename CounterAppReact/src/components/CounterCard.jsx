4a8026af36dbeb537a868957dbbb8aa5933ef74e
rajyadav <vector36razz@gmail.com>
2025-03-14 23:39:36 +0530

    counter app updated

--- CounterAppReact/src/components/CounterCard.jsx
+++ CounterAppReact/src/components/CounterCard.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import "./CounterCard.css";
 
 const CounterCard = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className="container">
-      <p>Clicked this times</p>
-      <button>Click</button>
+      <p>Clicked {count} times</p>
+      <button
+        id="counter-btn"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click
+      </button>
     </div>
   );
 };
