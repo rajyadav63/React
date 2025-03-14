11420275f9f76e23888ecc05dc58e65ef067bc39
rajyadav <vector36razz@gmail.com>
2025-03-14 23:15:40 +0530

    Counter app in react added

---
+++ CounterAppReact/src/App.jsx
@@ -0,0 +1,12 @@
+import CounterCard from "./components/CounterCard";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <CounterCard />
+    </>
+  );
+}
+
+export default App;
---
+++ CounterAppReact/src/components/CounterCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CounterCard = () => {
+  return (
+    <div className="container">
+      <p>Clicked this times</p>
+      <button>Click</button>
+    </div>
+  );
+};
+
+export default CounterCard;
